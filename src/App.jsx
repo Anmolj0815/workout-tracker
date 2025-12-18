9cf48c42a872edea0576a6498cdbfdf44ba2ee54
Anmol Jain <122198912+Anmolj0815@users.noreply.github.com>
2025-12-18 12:50:35 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WorkoutTracker from './components/WorkoutTracker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WorkoutTracker />
+    </div>
+  );
+}
+
+export default App;
